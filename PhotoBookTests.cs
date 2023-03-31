0f2585996a81766a54acbfa84b8d863821c6b4bb
Paarkavi Priya S <68461595+paarkavisaravanan19@users.noreply.github.com>
2023-03-31 14:43:57 +0530

    Add files via upload

---
+++ PhotoBookTests.cs
@@ -0,0 +1,41 @@
+﻿using Hands_On;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hands_On_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DefaultPhotoBook()
+        {
+            PhotoBook obj = new PhotoBook();
+            int pageCount = obj.GetNumberPages();
+            Assert.AreEqual(pageCount, 15);
+        }
+
+        [TestMethod]
+        public void PhotoBookTesting()
+        {
+            PhotoBook obj = new PhotoBook(32);
+            int pageCount = obj.GetNumberPages();
+            Assert.AreEqual(pageCount, 32);
+        }
+
+        [TestMethod]
+        public void LargePhotoBookTesting()
+        {
+            PhotoBook obj = new PhotoBook(200);
+            int pageCount = obj.GetNumberPages();
+            Assert.AreEqual(pageCount, 200);
+        }
+
+        [TestMethod]
+        public void AlbumBookTesting()
+        {
+            AddPhotoBook obj1 = new AddPhotoBook();
+            int pageCount = obj1.GetNumberPages();
+            Assert.AreEqual(pageCount, 64);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Hands_On
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhotoBook obj = new PhotoBook();
+            int pageCount = obj.GetNumberPages();
+            Console.WriteLine("pages: "+ pageCount);
+            PhotoBook obj1 = new PhotoBook(32);
+            int pageCount1 = obj1.GetNumberPages();
+            Console.WriteLine("pages after adding: " + pageCount1);
+            PhotoBook obj2 = new PhotoBook(200);
+            int pageCount2 = obj2.GetNumberPages();
+            Console.WriteLine("Large photo book pages: " + pageCount2);
+            AddPhotoBook obj3 = new AddPhotoBook();
+            int pageCount3 = obj3.GetNumberPages();
+            Console.WriteLine("Add photo book pages: " + pageCount3);
+        }
+    }
+    public class PhotoBook
+    {
+        int original_page;
+        //default constructor
+        public PhotoBook()
+        {
+            original_page = 16;
+        }
+        //parameterized constructor
+        public PhotoBook(int new_page)
+        {
+            this.original_page = new_page ;
+        }
+        public int GetNumberPages()
+        {
+            return original_page;
+        }
+
+        
+    }
+
+    public class AddPhotoBook 
+    {
+        int albumpage = 0;
+        //default constructor
+        public AddPhotoBook()
+        {
+            albumpage = 64;
+        }
+        //parameterized constructor
+        public int GetNumberPages()
+        {
+            return albumpage;
+        }
+    }
+
+}
